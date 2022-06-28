9bdfcd68b02ecdb0cfa3fe8687f010411fdf8cbb
juliakrtz <99036436+juliakrtz@users.noreply.github.com>
2022-06-28 16:45:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 from pcraster import *
 from pcraster.framework import *
+
+
+class MyFirstModel(DynamicModel, MonteCarloModel):
+  def __init__(self):
+    DynamicModel.__init__(self)
+    MonteCarloModel.__init__(self)
+    setclone('clone.map')
