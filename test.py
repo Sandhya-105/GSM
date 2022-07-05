57f65a4944bda429b25fd12d0fbfbc004c5bdc9c
juliakrtz <99036436+juliakrtz@users.noreply.github.com>
2022-07-05 14:01:29 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 from pcraster import *
 from pcraster.framework import *
 
-
-class MyFirstModel(DynamicModel, MonteCarloModel):
+class MyFirstModel(DynamicModel):
   def __init__(self):
     DynamicModel.__init__(self)
-    MonteCarloModel.__init__(self)
     setclone('clone.map')
+
+  def initial(self):
+    aUniformMap = uniform(1)
+    self.alive < .10
+    alive = uniform(1)<.10
+
+  def dynamic(self):
+    pass
+
+nrOfTimeSteps=1
+myModel = MyFirstModel()
+dynamicModel = DynamicFramework(myModel,nrOfTimeSteps)
+dynamicModel.run()
