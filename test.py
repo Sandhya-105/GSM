7ec5e3c3712370e7d4611d774b9dd47b5935fcdb
juliakrtz <99036436+juliakrtz@users.noreply.github.com>
2022-07-10 13:17:35 +0200

    changes to main

--- test.py
+++ test.py
@@ -8,21 +8,21 @@ class shrubmanagement(DynamicModel):
   def __init__(self):
     DynamicModel.__init__(self)
     setclone('mask.map')
-      #self.burn_rate = brnR
-      #self.graze_rate = grzR
+    self.burn_rate = 'brnR'
+    self.graze_rate = 'grzR'
       
   def initial(self):
     # initial distribution of shrubs 10%, grass 80%, empty 10%
     #judith's suggestion 
-    unimap = uniform()
-    vegetation = ifthenelse(unimap > 0.90, nominal(3), ifthenelse(unimap < 0.10, nominal(1), nominal(2)))
-    setclone(200,200,1,0,0)
+    #unimap = uniform()
+    #vegetation = ifthenelse(unimap > 0.90, nominal(3), ifthenelse(unimap < 0.10, nominal(1), nominal(2)))
+    #setclone(200,200,1,0,0)
     
-    #our testing
-    #map = uniform(1)
-    #initialdistr = ifthenelse(map < 0.1, nominal(3), ifthenelse(map>0.9, nominal(0), nominal(2))) 
-    #self.resultMap = self.readmap("initialdistr")
-    #self.report(initialdistr, "initial")
+    #other testing
+    map = uniform(1)
+    initialdistr = ifthenelse(map < 0.1, nominal(3), ifthenelse(map>0.9, nominal(0), nominal(2))) 
+    self.report(initialdistr, "initialdistr")
+    self.resultMap = self.readmap("initialdistr")
     
     #self.report(self.resultMap, "result_map")
     self.adultplants = 40
@@ -30,7 +30,7 @@ class shrubmanagement(DynamicModel):
     self.currentTimestep = 0
 
   def dynamic(self): # 0 empty, 1 fire, 2 grass, 3 shrub
-    self.currentTimestep = self.currentTimestep 
+    self.currentTimestep = self.currentTimestep +1 
     global shrubdevelopment
       
     #establish the parameters, detailed in the paper Table 1
@@ -73,51 +73,52 @@ class shrubmanagement(DynamicModel):
     self.report(prob_qBS, "Results/BS")
     self.report(prob_qBG, "Results/BG")
 
-
+    #t is the total number of cells for empty, burned, grass, and shrub
     self.resultMap = ifthenelse(self.resultMap == 1, 0, self.resultMap)
-    total_empty_cells = maptotal(scalar(self.resultMap == 0))
-    total_burn_cells = maptotal(scalar(self.resultMap == 1))
-    total_grass_cells= maptotal(scalar(self.resultMap == 2))
-    total_shrub_cells = maptotal(scalar(self.resultMap == 3))
+    t_ec = maptotal(scalar(self.resultMap == 0))
+    t_bc = maptotal(scalar(self.resultMap == 1))
+    t_gc= maptotal(scalar(self.resultMap == 2))
+    t_sc = maptotal(scalar(self.resultMap == 3))
 
-    global_prob_empty = total_empty_cells / self.totalCells #10 percent empty in intial time step
-    global_prob_grass = total_grass_cells / self.totalCells #80 percent grass in initial time step
-    global_prob_shrub = total_shrub_cells / self.totalCells #10 percent shrub in initial time step
+    #gp is global probability
+    gp_e = t_ec / self.totalCells #10 percent empty in intial time step
+    gp_g = t_gc / self.totalCells #80 percent grass in initial time step
+    gp_s = t_sc / self.totalCells #10 percent shrub in initial time step
 
-    self.report(total_empty_cells, "Results/total_empty_cells")
-    self.report(total_burn_cells, "Results/total_burn_cells")
-    self.report(global_prob_empty, "Results/prob_empty_cells")
-    self.report(global_prob_grass, "Results/prob_grass_cells")
-    self.report(global_prob_shrub, "Results/prob_shrub_cells")
+    self.report(t_ec, "Results/t_ec")
+    self.report(t_bc, "Results/t_bc")
+    self.report(gp_e, "Results/gp_e")
+    self.report(gp_g, "Results/gp_g")
+    self.report(gp_s, "Results/gp_s")
 
 
     #writing the transition rules
     #Colonization of an empty cell by a shrub
-    transition_ES = ((1-prob_qES) * shrub_empty_rate + shrub_empty_reprd) * prob_qES*(1-self.graze_rate)
+    transition_ES = ((1-prob_qSE) * shrub_empty_rate + shrub_empty_reprd) * prob_qSE
     
     #Colonization of an empty cell by grass
-    transition_EG = grass_empty_clonal_rate * prob_qEG + grass_seeds_empty_rate * global_prob_grass
+    transition_EG = grass_empty_clonal_rate * prob_qGE + grass_seeds_empty_rate * gp_g
     
     #Colonization of grass cells by shrub
-    transition_GS = ((1-prob_qGS) *shrub_grass_rate + shrub_grass_reprd) * prob_qGS*(1-self.graze_rate)
+    transition_GS = ((1-prob_qSG) *shrub_grass_rate + shrub_grass_reprd) * prob_qSG
     
     #Mortality rates
     shrub_death = shrub_mortality_rate + shrub_compete_rate * prob_qSS
     grass_death = grass_mortality_rate
     
     #Colonization of burned cells by shrub
-    transition_BS = ((1-prob_qBS) * shrub_burned_rate + shrub_empty_reprd) * prob_qBS*(1-self.graze_rate)
+    transition_BS = ((1-prob_qSB) * shrub_burned_rate + shrub_empty_reprd) * prob_qSB
     
     #Colonization of burned cells by grass
-    transition_BG = grass_empty_clonal_rate * prob_qBG + grass_seeds_empty_rate * global_prob_grass
+    transition_BG = grass_empty_clonal_rate * prob_qGB + grass_seeds_empty_rate * gp_g
 
 
     self.report(transition_ES, "Results/transES")
     self.report(transition_EG, "Results/transEG")
     self.report(transition_GS, "Results/transGS")
-    self.report(shrub_death, "Results/shrubdeath")
+    self.report(shrub_death, "Results/s_death")
 
-nrOfTimeSteps=1
+nrOfTimeSteps=20
 myModel = shrubmanagement() 
 dynamicModel = DynamicFramework(myModel,nrOfTimeSteps)
 dynamicModel.run()
